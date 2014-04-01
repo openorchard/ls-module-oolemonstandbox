61e42b241327f45db525865d89a7d1824ea35d11
ehlovader <joe.at.trends+github@gmail.com>
2014-04-01 12:33:23 -0400

    Commented out the deprecated functions which aren't being used.

--- classes/oolemonstandbox_module.php
+++ classes/oolemonstandbox_module.php
@@ -20,20 +20,20 @@
 		/*
 		 * Awaiting Deprecation
 		 */
-		public function listSettingsItems()
-		{
-			return $this->list_settings_items();
-		}
+		// public function listSettingsItems()
+		// {
+		// 	return $this->list_settings_items();
+		// }
 
 		protected function createModuleInfo()
 		{
 			return $this->create_module_info();
 		}
 		
-		public function subscribeEvents()
-		{
-			return $this->subscribe_events();
-		}
+		// public function subscribeEvents()
+		// {
+		// 	return $this->subscribe_events();
+		// }
 
 
 	}
